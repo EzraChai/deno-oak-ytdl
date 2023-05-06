3880c51e37aab5eda89ec4f34c31bcaa700ecdb3
Ezra Chai <juanzhe2@gmail.com>
2023-05-06 11:57:15 +0800

    update audio quality

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { Router } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 import { encodeUrl } from "https://deno.land/x/oak@v12.4.0/util.ts";
 import { httpErrors } from "https://deno.land/x/oak@v12.4.0/mod.ts";
-import NodeID3 from "npm:node-id3@0.2.6";
+// import NodeID3 from "npm:node-id3@0.2.6";
 
 import { getBasicInfo, ytdl } from "https://deno.land/x/ytdl_core/mod.ts";
 
@@ -56,12 +56,12 @@ router.post("/download", async (context) => {
     context.response.headers.set("Content-Disposition", disposition);
 
     // Pipe the video stream to the response
-    const tags = {
-      title: videoInfo.videoDetails.title,
-      // artist: videoInfo.videoDetails.author,
-      APIC: videoInfo.videoDetails.thumbnails[0].url,
-    };
-    await NodeID3.create(tags, audio);
+    // const tags = {
+    //   title: videoInfo.videoDetails.title,
+    //   // artist: videoInfo.videoDetails.author,
+    //   APIC: videoInfo.videoDetails.thumbnails[0].url,
+    // };
+    // await NodeID3.create(tags, audio);
     context.response.body = audio;
   } catch (_e) {
     throw new httpErrors.BadRequest("Invalid YouTube URL");
