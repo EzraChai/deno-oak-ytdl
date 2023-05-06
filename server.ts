bca8d6422c9afcf8276288231d6e2039627da4bc
Ezra Chai <juanzhe2@gmail.com>
2023-05-06 11:53:11 +0800

    added id3 (maybe)

--- server.ts
+++ server.ts
@@ -6,12 +6,9 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { Router } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 import { encodeUrl } from "https://deno.land/x/oak@v12.4.0/util.ts";
 import { httpErrors } from "https://deno.land/x/oak@v12.4.0/mod.ts";
+import NodeID3 from "npm:node-id3@0.2.6";
 
-import {
-  getBasicInfo,
-  validateURL,
-  ytdl,
-} from "https://deno.land/x/ytdl_core/mod.ts";
+import { getBasicInfo, ytdl } from "https://deno.land/x/ytdl_core/mod.ts";
 
 const router = new Router();
 
@@ -47,6 +44,7 @@ router.post("/download", async (context) => {
     const videoInfo = await getBasicInfo(value.url);
     const audio = await ytdl(value.url, {
       filter: "audioonly",
+      quality: "highestaudio",
     });
     // Set response headers
     context.response.status = 200;
@@ -58,6 +56,12 @@ router.post("/download", async (context) => {
     context.response.headers.set("Content-Disposition", disposition);
 
     // Pipe the video stream to the response
+    const tags = {
+      title: videoInfo.videoDetails.title,
+      // artist: videoInfo.videoDetails.author,
+      APIC: videoInfo.videoDetails.thumbnails[0].url,
+    };
+    await NodeID3.create(tags, audio);
     context.response.body = audio;
   } catch (_e) {
     throw new httpErrors.BadRequest("Invalid YouTube URL");
